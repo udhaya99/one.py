ed5d434e491e3757448d5c4a283354117312b56c
udhaya99 <52540980+udhaya99@users.noreply.github.com>
2019-07-09 21:28:19 +0530

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,6 @@
+x=input()
+list=['a','b','c','d','e','f','g','h','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+if x in list:
+  print("Alphabet")
+else:
+  print("No")
