27c2a15a553adad84afa81bca2f0f8c46d360a37
udhaya99 <52540980+udhaya99@users.noreply.github.com>
2019-07-09 18:13:09 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -2,8 +2,8 @@ x=input()
 list=['a','e','i','o','u']
 if len(x)<2:
   if x in list:
-    print("Vowels")
+    print("Vowel")
   else:
-    print("Consonants")
+    print("Consonant")
 else:
   print("invalid")
