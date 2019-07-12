7286cdf81e9255e84451a9145b05bdc8f77c9675
udhaya99 <52540980+udhaya99@users.noreply.github.com>
2019-07-12 16:38:15 +0530

    Update e7.py

--- e7.py
+++ e7.py
@@ -1,11 +1,11 @@
-a,b=map(int,input().split())
-for i in range(a,b):
-    sum=0
-    temp=i
-    while(temp>0):
-       c=temp%10
-       sum+=c**3
-       temp//10
-    if(i==sum):
-        print(sum,end=" ")
-   
+x,n=map(int,input().split())
+for i in range(x,n):
+  sum=0
+  temp=i
+  while(temp>0):
+    c=temp%10
+    sum=sum+c**3
+    temp=temp//10
+  if(i==sum):
+    print(sum,end=" ")
+     
